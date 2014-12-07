98310c531e544803e72f3dd814b3f04b6341b632
John Heroy <johnheroy@gmail.com>
2014-12-07 11:55:15 -0800

    In place quicksort implementation

---
+++ src/mergesort.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mergesort: mergesort
+};
--- src/quicksort.js
+++ src/quicksort.js
@@ -1,5 +1,5 @@
 module.exports = {
-  quicksort: naiveQuicksort
+  quicksort: quicksortInPlace
 };
 
 // simplest solution with extra storage arrays
@@ -25,6 +25,46 @@ function naiveQuicksort(array){
 }
 
 // in-place sort
-function quicksortInPlace(array){
+function quicksortInPlace(array, start, end){
+  if (array.length <= 1 || (end - start) < 1){
+    return;
+  }
 
+  var leftIndex, rightIndex;
+  if (start === undefined){
+    leftIndex = start = 0;
+  } else {
+    leftIndex = start;
+  }
+  if (end === undefined){
+    rightIndex = end = array.length - 1;
+  } else {
+    rightIndex = end;
+  }
+
+  var pivot = array[leftIndex];
+
+  while (leftIndex <= rightIndex){
+    while (array[leftIndex] < pivot){
+      leftIndex++;
+    }
+    while (array[rightIndex] > pivot){
+      rightIndex--;
+    }
+    if (leftIndex <= rightIndex){
+      swap(array, leftIndex, rightIndex);
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+  quicksortInPlace(array, start, rightIndex);
+  quicksortInPlace(array, leftIndex, end);
+  return array;
+}
+
+// utility
+function swap(array, i, j){
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 }
